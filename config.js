d754b6a353f7f89fa92f71bf65bf97772ac6c60a
André Padez <andre.padez@gmail.com>
2015-11-20 14:52:04 +0100

    changed environment variable names to allow PACKER_SERVER

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 var config = module.exports = {
-	repo: process.env.REPO,
-	branch: process.env.BRANCH
+	repo: process.env.MAIL_MAN_REPO || process.env.PACKER_SERVER_REPO,
+	branch: process.env.MAIL_MAN_BRANCH || || process.env.PACKER_SERVER_BRANCH
 }
 
-config.path = './' + config.repo.split('/').pop()
+config.path = '../' + config.repo.split('/').pop()
 config.remote = 'git@github.com:' + config.repo + '.git'
