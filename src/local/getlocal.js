a0f0a2cc4c058bcd97a05f797e1683ab1c54bb8a
Shawn Inder <shawn@vigour.io>
2016-01-08 09:31:06 -0500

    cpr -> ncp to avoid bug

--- src/local/getlocal.js
+++ src/local/getlocal.js
@@ -2,7 +2,7 @@
 
 var log = require('npmlog')
 var fs = require('vigour-fs-promised')
-var cpr = require('cpr')
+var ncp = require('ncp')
 
 module.exports = exports = function () {
   return fs.readFileAsync(this.config.localIgnore, 'utf8')
@@ -13,38 +13,32 @@ module.exports = exports = function () {
       log.info('packer-server', 'copying from', this.config.localPath, 'to', this.config.path)
       return copyDir(this.config.localPath,
         this.config.path,
-        { filter: (item) => {
-          console.log('filter')
-          var unprefixed = item.slice(this.config.localPath.length + 1)
-          console.log('item', unprefixed)
-          if (unprefixed.indexOf('.git') === 0) {
-            return false
-          } else {
-            var l = ignored.length
-            var found = false
-            for (var i = 0; i < l && !found; i += 1) {
-              if (unprefixed.indexOf(ignored[i]) === 0) {
-                found = true
+        { stopOnErr: true,
+          filter: (item) => {
+            var unprefixed = item.slice(this.config.localPath.length + 1)
+            if (unprefixed.indexOf('.git') === 0) {
+              return false
+            } else {
+              var l = ignored.length
+              var found = false
+              for (var i = 0; i < l && !found; i += 1) {
+                if (unprefixed.indexOf(ignored[i]) === 0) {
+                  found = true
+                }
               }
+              return !found
             }
-            if (!found) {
-              console.log('copying', unprefixed)
-            }
-            return !found
           }
-        }
-      })
+        })
     }, (reason) => {
       log.info('packer-server', 'copying all files from', this.config.localPath, 'to', this.config.path)
-      return copyDir(this.config.localPath, this.config.path, {})
+      return copyDir(this.config.localPath, this.config.path, { stopOnErr: true })
     })
 }
 
 function copyDir (src, dst, opts) {
   return new Promise(function (resolve, reject) {
-    console.log('calling cpr', src, dst, opts, 'cb')
-    cpr.cpr(src, dst, opts, function (err, files) {
-      console.log('aha')
+    ncp.ncp(src, dst, opts, function (err, files) {
       if (err) {
         reject(err)
       } else {
