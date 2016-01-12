d839803f6f52b3308b3b4d2810e8c6949051cd9f
Shawn Inder <shawn@vigour.io>
2016-01-12 14:55:08 -0500

    Add standardjs config and install btoa

--- src/github/checkifrepocloned.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-
-var log = require('npmlog')
-var fs = require('vigour-fs-promised')
-
-module.exports = exports = function () {
-  log.info('mail-man', 'checking if repo is already cloned')
-  return fs.existsAsync(this.config.path)
-}
