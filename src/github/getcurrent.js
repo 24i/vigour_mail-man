17171282aa4837988940c248da6efbdf69c4cfa9
Shawn Inder <shawn@vigour.io>
2016-01-14 13:34:14 -0500

    getCurrent eventually returns what it got from the command instead of this.state[target].sha, which could have changed since it was set

--- src/github/getcurrent.js
+++ src/github/getcurrent.js
@@ -5,14 +5,15 @@ var _trimRight = require('lodash/string/trimRight')
 var spawn = require('vigour-spawn')
 
 module.exports = exports = function (target) {
+  var sha
   return spawn('git rev-parse HEAD', { getOutput: true, cwd: path.join(this.config.path, target) })
     .then((out) => {
-      var sha = _trimRight(out)
+      sha = _trimRight(out)
       this.state[target].sha = sha
       return this.stateManager.save(this.state)
     })
     .then(() => {
-      return this.state[target].sha
+      return sha
     })
     .catch((reason) => {
       if (reason.code === 'ENOENT') {
